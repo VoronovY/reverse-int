c2d8b6741d444bb3eafed12f1f0f3b44f0c8ac0c
VoronovY <voronov.yuri87@gmail.com>
2023-01-05 19:57:55 +0100

    feat: resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let result = 0;
+    while (n >= 1) {
+        result = result * 10 + (n % 10);
+        n = (n - (n % 10)) / 10;
+    }
+    return result;
+};
